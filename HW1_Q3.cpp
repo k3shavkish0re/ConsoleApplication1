9edb3ca7577de6c541883d5c2373a8ddec54a6fa
Keshav <44866479+k3shavkish0re@users.noreply.github.com>
2024-09-04 22:31:09 -0500

    Add files via upload

---
+++ HW1_Q3.cpp
@@ -0,0 +1,72 @@
+//#include<systemc.h>
+//
+//SC_MODULE(gas_station) {
+//	sc_event request1, request2, tank1_full, tank2_full;
+//	int tank1, tank2, filling;
+//	int request1_active = 0;
+//	int request2_active = 0;
+//	SC_CTOR(gas_station) {
+//		tank1 = 0;
+//		tank2 = 0;
+//		filling = false;
+//		SC_METHOD(attendant); sensitive << request1 << request2; dont_initialize();
+//		SC_THREAD(customer1);
+//		SC_THREAD(customer2);
+//	}
+//	void attendant();
+//	void customer1();
+//	void customer2();
+//};
+//
+//void gas_station::customer1() {
+//	while (true) {
+//		wait(SC_ZERO_TIME); 
+//		tank1 = 0;
+//		cout << "new request from customer1"<<endl;
+//		do {
+//			request1.notify();
+//			wait(tank1_full); // Use static sensitivity
+//		} while (tank1 == 0);
+//	}
+//}
+//void gas_station::attendant() {
+//	if (!filling) {
+//		filling = true;
+//		if (request1.triggered()) { request1_active = 1;}
+//		if (request2.triggered()) { request2_active = 1;}
+//		next_trigger(sc_time(10, SC_NS));
+//	}
+//	else {
+//		if (request1_active) {
+//			tank1 = 100;	//prevent infinite do-while loop in customer1
+//			cout << "tank1 filled at: " << sc_time_stamp()<<", current value of tank1 is "<<tank1 << endl;
+//			request1_active = 0;
+//			tank1_full.notify(SC_ZERO_TIME);
+//		}
+//		if (request2_active) {
+//			tank2 = 100;	//prevent infinite do-while loop in customer2
+//			cout << "tank2 filled at: " << sc_time_stamp()<<". current value of tank2 is "<<tank2 << endl;
+//			request2_active = 0;
+//			tank2_full.notify(SC_ZERO_TIME);
+//		}
+//		filling = false;	//Assuming attendant can fill two tanks simultaneously
+//	}
+//}
+//
+//void gas_station::customer2() {
+//	while (true) {
+//		wait(SC_ZERO_TIME);
+//		tank2 = 0;
+//		cout << "new request from customer2"<<endl;
+//		do {
+//			request2.notify();
+//			wait(tank2_full); // dynamic sensitivity
+//		} while (tank2 == 0);
+//	}
+//}
+//
+//int sc_main(int argc, char* argv[]) {
+//	gas_station object("gas_station1");
+//	sc_start(sc_time(50, SC_NS));
+//	return 0;
+//}
